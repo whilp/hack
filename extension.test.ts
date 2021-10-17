0e6a2b69fad3215496926ddbdc1217ef102d9035
Will Maier <189851+whilp@users.noreply.github.com>
2021-10-17 13:53:11 -0700

    vscode

--- extension.test.ts
+++ extension.test.ts
@@ -1,10 +1,10 @@
-import { extension } from "extension.ts";
+import { activate } from "extension.ts";
 
 import { assertEquals } from "https://deno.land/std@0.111.0/testing/asserts.ts";
 
 Deno.test({
   name: "hello world #1",
   fn: () => {
-    assertEquals(extension(), 3);
+    assertEquals(activate(), null);
   },
 });
