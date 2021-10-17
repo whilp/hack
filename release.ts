76b5b0079e6ec2de1456b4d963617957d098d946
Will Maier <189851+whilp@users.noreply.github.com>
2021-10-17 15:05:48 -0700

    import

--- release.ts
+++ release.ts
@@ -1,4 +1,4 @@
-import * as vscode from "https://cdn.skypack.dev/@actions/github?dts";
+import { GitHub, context } from "https://cdn.skypack.dev/@actions/github?dts";
 
 const env = Deno.env.toObject();
 const github = new GitHub(env.GITHUB_TOKEN);
