110e165586aba740a0e11a226c7b0f56fd69d1ea
Will Maier <189851+whilp@users.noreply.github.com>
2021-10-17 14:51:46 -0700

    release

--- release.ts
+++ release.ts
@@ -2,3 +2,15 @@ import * as vscode from "https://cdn.skypack.dev/@actions/github?dts";
 
 const env = Deno.env.toObject();
 const github = new GitHub(env.GITHUB_TOKEN);
+const { owner: currentOwner, repo: currentRepo } = context.repo;
+
+const createReleaseResponse = await github.repos.createRelease({
+  owner,
+  report,
+  tag_name: "test",
+  name: "test",
+  body: "test",
+  draft: true,
+  prerelease: true,
+  target_commitish: context.sha,
+});
