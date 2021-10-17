11b086a5dfe6ccd6a442dcc49cc2d4295d7cec48
Will Maier <189851+whilp@users.noreply.github.com>
2021-10-17 14:26:44 -0700

    clean

--- extension.ts
+++ extension.ts
@@ -1,6 +1,5 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-// import * as vscode from "vscode";
 import * as vscode from "https://cdn.skypack.dev/vscode?dts";
 
 export function extension() {
