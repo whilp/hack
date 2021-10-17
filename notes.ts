82fe79df9a6e2d471345e75fcca99badceb44953
Will Maier <189851+whilp@users.noreply.github.com>
2021-10-17 14:26:36 -0700

    notes

---
+++ notes.ts
@@ -0,0 +1,9 @@
+/*
+
+- can factor deps into 3p.ts (or 3p/{a,b,c}.ts)
+- need package.json w/ engines.vscode
+- keep package.json in sync w/ deps
+
+https://github.com/microsoft/vscode-extension-samples/blob/main/configuration-sample/package.json
+
+*/
