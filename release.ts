897370204c8a8c5827435e563b1e4e72246ff11a
Will Maier <189851+whilp@users.noreply.github.com>
2021-10-17 18:42:12 -0700

    context

--- release.ts
+++ release.ts
@@ -1,12 +1,6 @@
-import {
-  getOctokit,
-  context,
-} from "https://esm.sh/@actions/github";
-// "https://cdn.skypack.dev/@actions/github?dts";
+import { getOctokit, context } from "https://esm.sh/@actions/github";
 
-const env = Deno.env.toObject();
-
-const github = getOctokit(env.GITHUB_TOKEN);
+const github = getOctokit(context.github.token);
 const { owner: currentOwner, repo: currentRepo } = context.repo;
 
 const createReleaseResponse = await github.repos.createRelease({
