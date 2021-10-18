91b76b138c42209e69ce8e716a3c71e56388833a
Will Maier <189851+whilp@users.noreply.github.com>
2021-10-17 18:44:13 -0700

    token?

--- release.ts
+++ release.ts
@@ -1,6 +1,6 @@
 import { getOctokit, context } from "https://esm.sh/@actions/github";
 
-const github = getOctokit(context.github.token);
+const github = getOctokit(context.token);
 const { owner: currentOwner, repo: currentRepo } = context.repo;
 
 const createReleaseResponse = await github.repos.createRelease({
