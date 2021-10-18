7336e7606b186b7b0014168776203d768ef01b2e
Will Maier <189851+whilp@users.noreply.github.com>
2021-10-17 17:50:33 -0700

    this now

--- notes.ts
+++ notes.ts
@@ -6,4 +6,9 @@
 
 https://github.com/microsoft/vscode-extension-samples/blob/main/configuration-sample/package.json
 
+commit cache ? 
+https://deno.land/manual/linking_to_external_code#but-what-if-the-host-of-the-url-goes-down-the-source-wont-be-available
+
+lock file
+
 */
--- release.ts
+++ release.ts
@@ -1,7 +1,11 @@
-import { GitHub, context } from "https://cdn.skypack.dev/@actions/github?dts";
+import {
+  getOctokit,
+  context,
+} from "https://cdn.skypack.dev/@actions/github?dts";
 
 const env = Deno.env.toObject();
-const github = new GitHub(env.GITHUB_TOKEN);
+
+const github = getOctokit(env.GITHUB_TOKEN);
 const { owner: currentOwner, repo: currentRepo } = context.repo;
 
 const createReleaseResponse = await github.repos.createRelease({
