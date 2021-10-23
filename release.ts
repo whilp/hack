612219c204ec49266f3e0ece039634f39d9752fa
Will Maier <189851+whilp@users.noreply.github.com>
2021-10-23 13:50:37 -0700

    env

--- release.ts
+++ release.ts
@@ -4,6 +4,8 @@ const env = Deno.env.toObject();
 const github = getOctokit(env.GITHUB_TOKEN);
 const { owner: currentOwner, repo: currentRepo } = context.repo;
 
+console.log(env);
+
 const createReleaseResponse = await github.repos.createRelease({
   owner,
   report,
