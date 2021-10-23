532d235b86972a551512f2deb8d84e822fc4370e
Will Maier <189851+whilp@users.noreply.github.com>
2021-10-23 14:18:10 -0700

    rest

--- release.ts
+++ release.ts
@@ -2,15 +2,13 @@ import { getOctokit } from "https://esm.sh/@actions/github";
 
 const env = Deno.env.toObject();
 
-const github = getOctokit(env.GITHUB_TOKEN);
-
 const githubRepository: string = env.GITHUB_REPOSITORY;
 var repo,
   owner = githubRepository.split("/", 2);
 
-console.log(repo, owner);
+const github = getOctokit(env.GITHUB_TOKEN);
 
-const createReleaseResponse = await github.repos.createRelease({
+const createReleaseResponse = await github.rest.repos.createRelease({
   owner,
   repo,
   tag_name: "test",
